804c9baf8ddc40aeef8581a89b650dd8dd01f3f9
mta13 <alimizady@gmail.com>
2023-05-26 16:20:12 +0330

    v 0.3

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Expence
+
+
+admin.site.register(Expence)
 
 # Register your models here.
--- web/models.py
+++ web/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 class Expence(models.Model):
-    pass
+    tittle = models.CharField(max_length=20)
+    date = models.DateTimeField()
+    amount = models.IntegerField()
+
 # Create your models here.
